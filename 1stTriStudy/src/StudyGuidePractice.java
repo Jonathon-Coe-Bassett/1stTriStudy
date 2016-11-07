6f1140279ae7b1149913c495b2acf7ea6dce38e5
Coe bassett <jonathon.bassett@mullenmail.com>
2016-11-07 09:31:09 -0700

    Initial Commit

---
+++ 1stTriStudy/src/StudyGuidePractice.java
@@ -0,0 +1,11 @@
+
+public class StudyGuidePractice
+	{
+		//Finals are on Nov 8 2016, today is Nov 7 2016. Better start studying in a program built solely to mess around with.
+		//First thing, Binary Search.
+		public static void main(String[] args)
+			{				
+
+			}
+
+	}
